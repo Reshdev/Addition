f95fa0254a66686bcaa86fe536cfc736bc3d5f2b
Administrator <Administrator@5CG9378HNN.mshome.net>
2023-06-14 20:25:10 +0530

    commit add

---
+++ Addition/src/add/add1.java
@@ -0,0 +1,15 @@
+package add;
+
+public class add1 {
+	
+	public static void main(String[] args) {
+		int a=13;
+		int b=23;
+		int c=a+b;
+
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
